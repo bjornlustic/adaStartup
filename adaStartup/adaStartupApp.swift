29b408d6bb7d2ce5e42f272d35971ded5579cdd6
bjornlustic <agentbjorn@gmail.com>
2025-05-09 15:29:19 -0700

    Initial Commit

---
+++ adaStartup/adaStartupApp.swift
@@ -0,0 +1,17 @@
+//
+//  adaStartupApp.swift
+//  adaStartup
+//
+//  Created by Bjorn Lustic on 5/9/25.
+//
+
+import SwiftUI
+
+@main
+struct adaStartupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ adaStartupTests/adaStartupTests.swift
@@ -0,0 +1,17 @@
+//
+//  adaStartupTests.swift
+//  adaStartupTests
+//
+//  Created by Bjorn Lustic on 5/9/25.
+//
+
+import Testing
+@testable import adaStartup
+
+struct adaStartupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
